8927e5a2269cb8bb61c651623c90a30a651987c5
Priyanshu <priyanshumaitra@gmail.com>
2021-06-20 15:28:57 +0530

    2nd commit

---
+++ L2_datatypes.cpp
@@ -0,0 +1,13 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int a = 78, b = 44;
+    float pi = 3.14;
+    char ch = 'p';
+
+    cout << "hello L2.\nValue of a is: " << a << ".\nvalue of b is: " << b << ".";
+    cout << "\nvalue of pi= " << pi << ".";
+    cout << "\n character = " << ch;
+    return 0;
+}
