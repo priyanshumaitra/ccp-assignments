8e0db734cc8a52f79e1ef0ffbf85cfa435444263
Priyanshu <priyanshumaitra@gmail.com>
2021-06-26 11:41:57 +0530

    Q1 updated

--- OOP/Assignment-3/Q1.cpp
+++ OOP/Assignment-3/Q1.cpp
@@ -1,29 +1,38 @@
 #include<iostream>
 using namespace std;
-class dist{
+class Distance{
+	private:
+		int feet, inch;
 	public:
-	int feet,inch,x,y,z;
-	void input(){
-		cout<<"Enter feet and inches: "<<"\n";
-		cin>>feet>>inch;
-	}
-	void show(){
-		cout<<"The distance is ";
-		cout<<feet<< " feet "<<inch<<" inch\n";
-	}
-	void sum(dist x,dist y){
-		feet=x.feet+y.feet;
-		inch=x.inch+y.inch;
-		if(inch>=12){
-			feet=feet+1;
-			inch=inch-12;
+		void set_distance(){
+			cout<<"Enter measurement in feet: ";
+			cin>>feet;
+			cout<<"Enter measurement in inches: ";
+			cin>>inch;
 		}
-	}
+		void get_distance(){
+			cout<<"Distance in feet is: "<<feet<<" ft."<<"and inches is: "<<inch<<" in."<<endl;
+		}
+		void add(Distance d1, Distance d2){
+			feet = d1.feet + d2.feet;
+			inch = d1.inch + d2.inch;
+			feet = feet + (inch / 12);
+			inch = inch % 12;
+		}
+		void sub(Distance d1, Distance d2){
+			feet = d1.feet - d2.feet;
+			inch = d1.inch - d2.inch;
+			feet = feet - (inch / 12);
+			inch = inch % 12;
+		}
 };
 int main(){
-	dist x,y,z;
-	x.input();
-	y.input();
-	z.sum(x,y);
-	z.show();
+	Distance d1, d2, d3, d4;
+	d1.set_distance();
+	d2.set_distance();
+	d3.add(d1, d2);
+	d4.sub(d1, d2);
+	d3.get_distance();
+	d4.get_distance();
+	return 0;
 }
